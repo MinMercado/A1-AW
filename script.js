550292385d2587cf90efe7f10c2a894c3c0195e9
Jazmin Mercado <jazminmerva@gmail.com>
2024-09-17 10:03:28 -0600

    1era edición

---
+++ script.js
@@ -0,0 +1,42 @@
+
+alert("Estoy probando JavaScript, ¡Hola! :)");
+
+let app = document.getElementById('app');
+
+let typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 75,
+});
+
+typewriter
+  .pauseFor(2500)
+  .typeString('Conoce México')
+  .pauseFor(300)
+  .deleteAll()
+  .typeString('Nuestra cultura,')
+  .pauseFor(1000)
+  .deleteAll()
+  .typeString('nuestras tradiciones')
+  .pauseFor(1000)
+  .deleteAll()
+  .typeString('y nuestras festividades.')
+  .pauseFor(1000)
+  .start();
+
+
+let app2 = document.getElementById('app2');
+
+let tw = new Typewriter(app2, {
+  loop: true,
+  delay: 75,
+});
+
+tw
+  .pauseFor(2500)
+  .typeString('¡Viva México!, ¡Viva la religión!, ¡Muera el mal Gobierno!')
+  .pauseFor(300)
+  .deleteAll()
+  .start();
+
+
+  
